14e6efb2440b051cb3ae8fad48604ba9713452bf
HoldenJC <holdenj_c@yahoo.com>
2019-06-03 16:46:27 -0700

    add scripts to alter user entered sentence in multiple ways, add clickable buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,79 @@
+var myFunction = function() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+}
+  // alert("I was clicked");
+};
+
 $(function() {
+
+  var userSentence = prompt('Please enter in a sentence: ');
+  alert(userSentence);
+
+  var firstLastCapital = function(userSentence) {
+    var firstLetter = userSentence.charAt(0).toUpperCase();
+    var lastLetter = userSentence.charAt(userSentence.length-1).toUpperCase();
+    var firstLastCapitalResult = firstLetter + lastLetter;
+    return firstLastCapitalResult;
+  };
+
+  var string1 = firstLastCapital(userSentence);
+  alert(string1);
+
+  var reverseString = function(string1) {
+    var result = string1.split('').reverse().join('');
+    return result;
+  };
+
+  var string2 = reverseString(string1);
+  alert(string2);
+
+  var concatenateOthers = function(userSentence) {
+    var temp1 = firstLastCapital(userSentence);
+    var temp2 = reverseString(firstLastCapital(userSentence));
+    var result = temp1 + temp2;
+    return result;
+  };
+
+  var string3 = concatenateOthers(userSentence);
+  alert(string3);
+
+  alert(userSentence + string3);
+
+  var countSentence = function(userSentence) {
+    var sentenceLength = userSentence.length;
+    var dividend = sentenceLength / 2;
+    var chosenLetter = userSentence.charAt(dividend);
+    return chosenLetter + userSentence;
+
+  };
+
+  var string4 = countSentence(userSentence);
+  alert(string4);
+
+  alert(reverseString(string4));
+
+
+
   $("h1").click(function() {
-    alert("This is a header.");
+    alert(userSentence);
   });
 
   $("p").click(function() {
-    alert("This is a paragraph.");
+    alert(reverseString(string4));
   });
 
-  $("img").click(function() {
-    alert("This is an image.");
+  $("#myButton").click(function(){
+    alert("Second Button was clicked too!");
   });
+
+
 });
+
+
 
 /*
 jQuery(document).ready(function() {
