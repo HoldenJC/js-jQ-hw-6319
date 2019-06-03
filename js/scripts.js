1b011566fc85732375f2b02543b39f81fbff08df
HoldenJC <holdenj_c@yahoo.com>
2019-06-03 13:39:56 -0700

    add initial HTML, javascript scripts, add jquery 3.4.1

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
+
+/*
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
+});
+*/
+
+/* BOTH the below and above function the same... $ and jQuery are interchangeable here
+
+$(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
+*/
